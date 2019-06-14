22e5c0c783a27a3bacf0bbe9fb2551a59735fac2
Marcin Radziszewski <marcincho@gmail.com>
2019-06-14 10:38:02 +0200

    DI for ApiClient

---
+++ AlfaRec.MVC/Connections/ApiClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace AlfaRec.MVC.Connections
+{
+    public partial class ApiClient
+    {
+        private readonly HttpClient httpClient;
+        public ApiClient(HttpClient client)
+        {
+            httpClient = client;
+        }
+    }
+}
--- AlfaRec.MVC/Startup.cs
+++ AlfaRec.MVC/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlfaRec.MVC.Connections;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -29,7 +30,7 @@ namespace AlfaRec.MVC
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            services.AddHttpClient<ApiClient>(c => c.BaseAddress = new Uri(Configuration.GetValue<string>("WebApiURI")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
